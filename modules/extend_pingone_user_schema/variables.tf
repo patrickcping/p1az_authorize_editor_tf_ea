a8949d75c386991119700c7d5cc364f4f9904dbe
Patrick Cowland <patrickcowland@pingidentity.com>
2025-02-14 11:59:39 +0000

    fix variable

--- modules/extend_pingone_user_schema/variables.tf
+++ modules/extend_pingone_user_schema/variables.tf
@@ -3,7 +3,7 @@ variable "pingone_environment_id" {
   description = "The PingOne environment ID that has the PingOne Authorize Editor API feature flag enabled"
 
   validation {
-    condition     = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.pingone_authorize_environment_id))
+    condition     = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.pingone_environment_id))
     error_message = "The PingOne Environment ID must be a valid PingOne resource ID (UUID format)."
   }
 }
